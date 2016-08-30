69130c2a99d7f846caa5853bf17179598f15f15f
Agadar <Agadar@users.noreply.github.com>
2016-08-30 13:01:49 +0200

    Condensed code

--- Week 1/Week1.ino
+++ Week 1/Week1.ino
@@ -2,30 +2,20 @@
  
 void setup() {    
  Serial.begin(baudRate);
-
- // Make pin 10 an output
- DDRB |= _BV(PINB2);
-
- // Set HIGH for pin 10
- PORTB |= _BV(PINB2);
-
- // Make pin 5 an output
-  DDRD |= _BV(PORTD5);
+ DDRB |= _BV(PINB2); // Make pin 10 an output
+ PORTB |= _BV(PINB2); // Set HIGH for pin 10
+ DDRD |= _BV(PORTD5); // Make pin 5 an output
 }
  
 void loop(){
-
- // Read the current value
- int readBit = bitRead(PINB, PINB2);
+ int readBit = bitRead(PINB, PINB2); // Read the current value
 
  if (readBit == LOW)
  {  
-  // Power the lamp at pin 5
-  PORTD |= _BV(PORTD5);
+  PORTD |= _BV(PORTD5); // Power the lamp at pin 5
  }
  else
  {
-  // IS MASKER!!!! 
-  PORTD &= B11011111;
+  PORTD &= B11011111; // Apply mask to set the 5th bit to 0
  }
 }
