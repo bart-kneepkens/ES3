da94ba9f88bcf26ff3935984ecfdc16a6d5840f3
Bart Kneepkens <bkneepkens@msn.com>
2016-08-30 13:24:14 +0200

    Ugly draft of complimentary features. FIX THIS SENPAI

--- Week 1/Week1/Week1.ino
+++ Week 1/Week1/Week1.ino
@@ -4,13 +4,20 @@ void setup() {
  Serial.begin(baudRate);
  DDRB |= _BV(PINB2); // Make pin 10 an output
  PORTB |= _BV(PINB2); // Set HIGH for pin 10
+
+ DDRB |= _BV(PINB4); // Make pin 12 an output
+ PORTB |= _BV(PINB4); // Set HIGH for pin 12
+  
+
+
+ DDRD |= _BV(PORTD6); // Make pin 6 an output
  DDRD |= _BV(PORTD5); // Make pin 5 an output
 }
  
 void loop(){
- int readBit = bitRead(PINB, PINB2); // Read the current value
+ int stateOfButton10 = bitRead(PINB, PINB2); // Read the current value
 
- if (readBit == LOW)
+ if (stateOfButton10 == LOW)
  {  
   PORTD |= _BV(PORTD5); // Power the lamp at pin 5
  }
@@ -18,4 +25,22 @@ void loop(){
  {
   PORTD &= ~_BV(PORTD5); // Apply mask to set the 5th bit to 0
  }
+
+ int stateOfButton12 = bitRead(PINB, PINB4); // Read the current value
+
+ if (stateOfButton12 == LOW)
+ {  
+  PORTD |= _BV(PORTD6); // Power the lamp at pin 5
+  Serial.println("Hello, world!");
+ }
+ else
+ {
+  PORTD &= ~_BV(PORTD6); // Apply mask to set the 5th bit to 0
+ }
+
+ if(stateOfButton10 == LOW && stateOfButton12 == LOW){
+  Serial.println("Both!!");
+  tone(3, 100, 10);
+ }
+ 
 }
