dfb5f0f89b5e727cc148b6ce23f873cc00c453c2
Bart Kneepkens <bkneepkens@msn.com>
2016-09-14 20:43:38 +0200

    Beam me up, scotty.

--- Week_3/Week_3_Watchdog/Week_3_Watchdog/Week_3_Watchdog.ino
+++ Week_3/Week_3_Watchdog/Week_3_Watchdog/Week_3_Watchdog.ino
@@ -1,6 +1,7 @@
 #define LEDPIN 13;
 #define FORMULTIPLIER 445;
 #include <avr/wdt.h>
+#include <avr/sleep.h>
 
 void setBit(volatile unsigned char* reg, int bitNr, bool value)
 {
@@ -29,48 +30,50 @@ void my_delay(volatile long ms) {
 }
 
 ISR(WDT_vect){
-  Serial.println("Watchdoge");
+  Serial.println("WDT ISR");
+  //SMCR |= (0<<0);            // Turn off the power-down mode.
 }
 
 void setup() {
   Serial.begin(9600);
-  
   setBit(&DDRB, 5, true);   // Set led pin 13 as an output.
   
   ADCSRA = 0;               // Turn off ADC.
 
   PRR = 0;                  // Turn off timers and Peripherals.
 
-
   /** Set the Watchdog interrupt and watchdog prescaler at 4sec **/
   cli();
-  //wdt_reset();
   
-  WDTCSR = (1<<4)|(1<<3); // B00011000    // Set up WDT interrupt.
+  WDTCSR = (1<<4)|(1<<3);         //(B00011000)    // Set up WDT interrupt.
   
-  WDTCSR = (1<<6)|(1<<3)|(1<<5);  // B01101000; // Start watchdog timer with 4sec prescaler.
+  WDTCSR = (1<<6)|(1<<3)|(1<<5);  //(B01101000) // Start watchdog timer with 4 second prescaler.
+  
   sei();
 }
 
+void feedDog(){
+  MCUSR &= (0<<3);
+  WDTCSR &= (0<<3);
+}
+
 void loop() {
-  setPrescaler(1);
+  
+  //feedDog();
+  wdt_reset();
+  
+  /*  Blink 2 times.  */
   setBit(&PORTB, 5, true);  // Set LED pin 13 to HIGH.
-  
-  my_delay(2000);
-
-  setPrescaler(4);
+  my_delay(500);
   setBit(&PORTB, 5, false); // Set LED pin 13 to LOW.
-  
-  my_delay(2000);
-
-  setPrescaler(16);
+  my_delay(500);
   setBit(&PORTB, 5, true);  // Set LED pin 13 to HIGH.
-  
-  my_delay(2000);
-  
-  setPrescaler(256);
+  my_delay(500);
   setBit(&PORTB, 5, false); // Set LED pin 13 to LOW.
-  
-  my_delay(2000);
+  my_delay(500);
+
+  /*  Power down mode.  */
+  SMCR |= (1<<2)|(1<<0);
+  sleep_cpu();
 }
 
