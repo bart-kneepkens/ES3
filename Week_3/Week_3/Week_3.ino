2689cbad7026e08da2830610803e27efa1ad63f8
Bart Kneepkens <bkneepkens@msn.com>
2016-09-15 20:09:54 +0200

    Final code cleanup.

--- Week_3/Week_3/Week_3.ino
+++ Week_3/Week_3/Week_3.ino
@@ -1,56 +1,33 @@
-#define LEDPIN 13;
 #define FORMULTIPLIER 445;
 
-#include <avr/Power.h>
-
-void setBit(volatile unsigned char* reg, int bitNr, bool value)
-{
-  if (value)
-  {
-    *reg |= _BV(bitNr);
-  }
-  else
-  {
-    *reg &= ~_BV(bitNr);
-  } 
+/*  NOTE: Call with values 1-8  */
+void setPrescaler(int newValue){ 
+  CLKPR = 0x80;            // Enable the change bit.
+  CLKPR = newValue;        // Set the prescaler.
 }
-
-// NOTE: Call with values 1/8
-void setPrescaler(int newValue){
-    CLKPR = 0x80;            // Enable the change bit.
-    CLKPR = newValue;        // Set the prescaler.
-}
-
 
 void my_delay(volatile long ms) {
-  long maximum = ms * FORMULTIPLIER
-  
-  maximum = maximum / pow(2,CLKPR);
-  
-  for (volatile long i = 0; i < maximum; i++) {}
+  long maximum = ms * FORMULTIPLIER;
+  maximum = maximum / pow(2,CLKPR);               // Since the CLKPR register is only 1-8, we have to divide by the CLKPR'th power of 2.
+  for (volatile long i = 0; i < maximum; i++) {}  // Empty for-loop. (Busy waiting).
 }
 
 void setup() {
-  setBit(&DDRB, 5, true);   // Set led pin 13 as an output.
-  
+  DDRB |= (1<<DDB5);        // Set led pin 13 as an output.
   ADCSRA = 0;               // Turn off ADC.
-
   PRR = 0;                  // Turn off timers and Peripherals.
 }
 
 void blink(int delay){
-  setBit(&PORTB, 5, true); // Set led pin 13 to HIGH.
+  PORTB |= (1<<PORTB5);       // Set led pin 13 to HIGH.
   my_delay(delay);
-  setBit(&PORTB, 5, false);  // Set led pin 13 to LOW.
+  PORTB &= ~(1<<PORTB5);      // Set led pin 13 to LOW.
   my_delay(delay);
 }
 
 void loop() {
-
-
    blink(2000);
    blink(2000);
-
    setPrescaler(8);
 }
 
--- Week_3/Week_3_Watchdog/Week_3_Watchdog/Week_3_Watchdog.ino
+++ Week_3/Week_3_Watchdog/Week_3_Watchdog/Week_3_Watchdog.ino
@@ -1,67 +1,42 @@
-#define LEDPIN 13;
 #define FORMULTIPLIER 445;
-#include <avr/wdt.h>
 #include <avr/sleep.h>
 
-void setBit(volatile unsigned char* reg, int bitNr, bool value)
-{
-  if (value)
-  {
-    *reg |= _BV(bitNr);
-  }
-  else
-  {
-    *reg &= ~_BV(bitNr);
-  } 
+/*  NOTE: Call with values 1-8  */
+void setPrescaler(int newValue){ 
+  CLKPR = 0x80;            // Enable the change bit.
+  CLKPR = newValue;        // Set the prescaler.
 }
-
 
 void my_delay(volatile long ms) {
-  long maximum = ms * FORMULTIPLIER
-  
-  maximum = maximum / pow(2,CLKPR);
-  
-  for (volatile long i = 0; i < maximum; i++) {}
+  long maximum = ms * FORMULTIPLIER;
+  maximum = maximum / pow(2,CLKPR);               // Since the CLKPR register is only 1-8, we have to divide by the CLKPR'th power of 2.
+  for (volatile long i = 0; i < maximum; i++) {}  // Empty for-loop. (Busy waiting).
 }
 
-
-ISR(WDT_vect){
-  Serial.println("ISR");
-  } // The cpu_sleep is disabled by default when ISR is called.
+ISR(WDT_vect){}             // The cpu_sleep is disabled by default when ISR is called.
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Setup");
-  setBit(&DDRB, 5, true);   // Set led pin 13 as an output.
-  
+  DDRB |= (1<<DDB5);        // Set led pin 13 as an output.
   ADCSRA = 0;               // Turn off ADC.
-
   PRR = 0;                  // Turn off timers and Peripherals.
 
-  /** Set the Watchdog interrupt and watchdog prescaler at 4sec **/
   cli();
-  
   WDTCSR = (1<<4)|(1<<3);         //(B00011000) Set up WDT interrupt.
-  
   WDTCSR = (1<<6)|(1<<3)|(1<<5);  //(B01101000) Start watchdog timer with 4 second prescaler.
- 
   sei();
 }
 
 void blink(int delay){
-  setBit(&PORTB, 5, true); // Set led pin 13 to HIGH.
+  PORTB |= (1<<PORTB5);       // Set led pin 13 to HIGH.
   my_delay(delay);
-  setBit(&PORTB, 5, false);  // Set led pin 13 to LOW.
+  PORTB &= ~(1<<PORTB5);      // Set led pin 13 to LOW.
   my_delay(delay);
 }
 
 void loop() {
-  /*  Blink 2 times.  */
   blink(500);
   blink(500);
-
-  /*  Power down mode.  */
-  SMCR |= (1<<2)|(1<<0);
-  sleep_cpu();
+  SMCR |= (1<<2)|(1<<0);      // Power-down mode. 
+  sleep_cpu();  
 }
 
